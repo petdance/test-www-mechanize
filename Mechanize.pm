c73890081de0bb11dcf302ed47612a1b36d053a1
petdance <petdance@fa6c7475-4f37-0410-a643-9d661a9e9720>
2009-08-17 19:49:43 +0000

    updated copyright and licensing

--- Mechanize.pm
+++ Mechanize.pm
@@ -1330,10 +1330,20 @@ and Pete Krawczyk for patches.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2004-2008 Andy Lester, all rights reserved.
+Copyright 2004-2009 Andy Lester.
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or
+modify it under the terms of either:
+
+=over 4
+
+=item * the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+=item * the Artistic License version 2.0.
+
+=back
 
 =cut
 
