4182a3485268f50993344447dbda8df413d1eb6e
petdance <petdance@fa6c7475-4f37-0410-a643-9d661a9e9720>
2010-04-01 21:24:32 +0000

    tools for testing the hanging

---
+++ t/HangServer.pm
@@ -0,0 +1,93 @@
+package TestServer;
+
+use warnings;
+use strict;
+
+BEGIN {
+    delete $ENV{http_proxy}; # All our tests are running on localhost
+}
+
+use base 'HTTP::Server::Simple::CGI';
+
+use Carp ();
+
+our $pid;
+
+our $fh;
+
+sub new {
+    my $class = shift;
+
+open $fh, '>', '/home/andy/test-www-mechanize/cgidump.txt' or die;
+print {$fh} "started logging\n";
+    die 'An instance of TestServer has already been started.' if $pid;
+
+    # XXX This should really be a random port.
+    return $class->SUPER::new(13432, @_);
+}
+
+sub run {
+    my $self = shift;
+
+    $pid = $self->SUPER::run(@_);
+
+    $SIG{__DIE__} = \&stop;
+
+    return $pid;
+}
+
+sub handle_request {
+    my $self = shift;
+    my $cgi  = shift;
+
+    use Data::Dumper; local $Data::Dumper::Sortkeys=1;
+    my $response = Dumper($cgi);
+    print "HTTP/1.0 200 OK\r\n";
+    print "Content-Type: text/html\r\nContent-Length: ", length($response), "\r\n\r\n", $response;
+    return;
+
+    my $file = (split( /\//,$cgi->path_info))[-1]||'index.html';
+    $file    =~ s/\s+//g;
+
+    my $filename = "t/html/$file";
+    if ( -r $filename ) {
+        if (my $response=do { local (@ARGV, $/) = $filename; <> }) {
+            print "HTTP/1.0 200 OK\r\n";
+            print "Content-Type: text/html\r\nContent-Length: ", length($response), "\r\n\r\n", $response;
+            return;
+        }
+    }
+
+    print "HTTP/1.0 404 Not Found\r\n\r\n";
+
+    return;
+}
+
+sub background {
+    my $self = shift;
+
+    $pid = $self->SUPER::background()
+        or Carp::confess( q{Can't start the test server} );
+
+    sleep 1; # background() may come back prematurely, so give it a second to fire up
+
+    return $pid;
+}
+
+sub root {
+    my $self = shift;
+    my $port = $self->port;
+
+    return "http://localhost:$port";
+}
+
+sub stop {
+    if ( $pid ) {
+        kill( 9, $pid ) unless $^S;
+        undef $pid;
+    }
+
+    return;
+}
+
+1;
