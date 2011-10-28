93c9d6ebac9503e6aa9de0386a95b4bd71ada836
Andy Lester <andy@petdance.com>
2011-10-28 10:18:10 -0500

    Fix t/autolint.t and update to 1.38

--- Mechanize.pm
+++ Mechanize.pm
@@ -9,11 +9,11 @@ Test::WWW::Mechanize - Testing-specific WWW::Mechanize subclass
 
 =head1 VERSION
 
-Version 1.36
+Version 1.38
 
 =cut
 
-our $VERSION = '1.36';
+our $VERSION = '1.38';
 
 =head1 SYNOPSIS
 
